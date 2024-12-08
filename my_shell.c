7e81850c1dd7f7fb5da93a1b7b56fc4e7ad21435
KshitijPingle <111930104+KshitijPingle@users.noreply.github.com>
2024-12-07 19:20:13 -0800

    Got shell to start to work and exit

---
+++ my_shell.c
@@ -0,0 +1,31 @@
+// Add a header later
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE 1000
+int main(int argc, char *argv[])
+{
+    char line[MAX_LINE];
+    while(1)
+    {
+        printf("my_shell> ");
+        fgets(line, MAX_LINE, stdin);
+        
+
+        // Check if the user wants to exit the shell
+        if ((strcmp(line, "exit\n") == 0) || (strcmp(line, "quit\n") == 0))
+        {
+            printf("Exiting my_shell\n");
+            break;
+        }
+
+        // Remove the newline character from the end of the line
+        line[strcspn(line, "\n")] = 0;
+
+        printf("You entered: '%s'\n", line);
+    }
+
+    return 0;
+}
